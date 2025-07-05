e4789499802008c2684902baa8cd7cd2a32d2669
cauagomes06 <cau201812@gmail.com>
2025-07-05 14:29:48 -0300

    ATIVIDADE 14 - PROJETO DE GERENCIAMENTO DE CURSO LIGADO COM BANCO DE DADOS e DAO

---
+++ src/main/java/br/com/ifba/curso/dao/CursoIDao.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package br.com.ifba.curso.dao;
+
+import br.com.ifba.curso.entity.Curso;
+import br.com.ifba.infrastructure.dao.GenericIDao;
+import java.util.List;
+
+
+/**
+ *
+ * @author Cauã
+ */
+public interface CursoIDao extends GenericIDao<Curso> {
+    
+    public abstract List<Curso> findbyName(String nomeBuscado);
+}
